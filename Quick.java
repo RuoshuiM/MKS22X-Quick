4ab7245a6e24f5564e1b038a1f38ff49655b42ca
Ruoshui <ruoshuim@gmail.com>
2019-03-21 06:40:09 -0400

    Updates quicksort with insertion

--- Quick.java
+++ Quick.java
@@ -1,8 +1,8 @@
+import java.util.Arrays;
 import java.util.Random;
 
-import java.util.Arrays;
-
 public class Quick {
+    final static int INSERSIONSORT_THRESHOLD = 47;
 
     /**
      * @return the value that is the kth smallest value of the array.
@@ -37,6 +37,7 @@ public class Quick {
      * @return the index of the final position of the pivot element.
      */
     static int partition(int[] data, int start, int end) {
+
         Random rand = new Random();
         int pivotIndex = rand.nextInt(end + 1 - start) + start;
         int pivot = data[pivotIndex];
@@ -62,6 +63,25 @@ public class Quick {
 
         // if pivot is not at index "start", then it is at "start - 1"
         return pivot == data[start] ? start : start - 1;
+    }
+
+    static void insertionsort(int[] arr, int low, int high) {
+      // i is the current number to be inserted, high is inclusive
+      for (int i = low + 1; i <= high; i++) {
+        int cur = arr[i];
+        // loop through all the sorted elements
+        for (int j = i - 1; j >= low; j--) {
+          // if current number is greater than the sorted value, then cur is in sorted position
+          // System.out.format("cur: %d, future: %d%ncurIndex: %d, futureIndex:%d%n", cur, arr[j], i, j);
+          if (cur >= arr[j]) {
+            break;
+          } else {
+            aswap(arr, j + 1, j);
+            // System.out.format("Switched, first: %d, second: %d%n", arr[i], arr[j]);
+          }
+          // System.out.format("Switched: %s%n", Arrays.toString(arr));
+        }
+      }
     }
 
     private static void aswap(int[] arr, int a, int b) {
@@ -111,7 +131,6 @@ public class Quick {
             }
         }
 
-
         return new int[] { start, end };
     }
 
@@ -123,7 +142,13 @@ public class Quick {
     }
 
     private static void quicksort(int[] data, int start, int end) {
-        if (end - start < 1) return;
+//        if (end <= start)
+//            return;
+
+        if (end - start + 1 < INSERSIONSORT_THRESHOLD) {
+            insertionsort(data, start, end);
+            return;
+        }
 
         int[] re = partitionDutch(data, start, end);
         int lt = re[0];
@@ -131,65 +156,63 @@ public class Quick {
         quicksort(data, start, lt - 1);
         quicksort(data, gt + 1, end);
     }
-
-    public static void main(String[] args) {
-
-      System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-int[]MAX_LIST = {1000000000,500,10};
-for(int MAX : MAX_LIST){
-  for(int size = 31250; size < 2000001; size*=2){
-    long qtime=0;
-    long btime=0;
-    //average of 5 sorts.
-    for(int trial = 0 ; trial <=5; trial++){
-      int []data1 = new int[size];
-      int []data2 = new int[size];
-      for(int i = 0; i < data1.length; i++){
-        data1[i] = (int)(Math.random()*MAX);
-        data2[i] = data1[i];
-      }
-      long t1,t2;
-      t1 = System.currentTimeMillis();
-      Quick.quicksort(data2);
-      t2 = System.currentTimeMillis();
-      qtime += t2 - t1;
-      t1 = System.currentTimeMillis();
-      Arrays.sort(data1);
-      t2 = System.currentTimeMillis();
-      btime+= t2 - t1;
-      if(!Arrays.equals(data1,data2)){
-        System.out.println("FAIL TO SORT!");
-        System.exit(0);
-      }
+    
+    public static void quicksortWithoutInsertion(int[] data) {
+        quicksortWithoutInsertion(data, 0, data.length - 1);
     }
-    System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-  }
-  System.out.println();
-}
+    
+    private static void quicksortWithoutInsertion(int[] data, int start, int end) {
+        if (end <= start)
+            return;
+//
+//        if (end - start + 1 < INSERSIONSORT_THRESHOLD) {
+//            insertionsort(data, start, end);
+//        }
 
-      // testQuicksortRandVals();
+        int[] re = partitionDutch(data, start, end);
+        int lt = re[0];
+        int gt = re[1];
+        quicksortWithoutInsertion(data, start, lt - 1);
+        quicksortWithoutInsertion(data, gt + 1, end);
     }
-/*
-    public static void testQuicksortRandVals() {
-        final int size = 1000000;
-        for (int i = 0; i < 5; i++) {
-
-            int[] arr = TestTools.generateIntArray(size, 0);
-            int[] sorted = arr.clone();
-
-            long sysTime = System.nanoTime();
-            Arrays.sort(sorted);
-            sysTime = System.nanoTime() - sysTime;
-
-            long myTime = System.nanoTime();
-            Quick.quicksort(arr);
-            myTime = System.nanoTime() - myTime;
-
-            System.out.format("SysTime: %d, myTime: %d%nFaster: %s%n", sysTime, myTime, (sysTime >= myTime?"mine":"sys"));
-
-            TestTools.testIntArraysEquals(sorted, arr);
-
-	}
+    
+    public static void main(String...args) {
+        compareSorts();
     }
-    */
+    
+    public static void compareSorts() {
+        System.out.println("Size\t\tMax Value\tinsertionquick/justquick ratio ");
+        int[] MAX_LIST = { 1000000000, 500, 10 };
+        for (int MAX : MAX_LIST) {
+            for (int size = 31250; size < 2000001; size *= 2) {
+                long qtime = 0;
+                long btime = 0;
+                // average of 5 sorts.
+                for (int trial = 0; trial <= 5; trial++) {
+                    int[] data1 = new int[size];
+                    int[] data2 = new int[size];
+                    for (int i = 0; i < data1.length; i++) {
+                        data1[i] = (int) (Math.random() * MAX);
+                        data2[i] = data1[i];
+                    }
+                    long t1, t2;
+                    t1 = System.currentTimeMillis();
+                    quicksort(data2);
+                    t2 = System.currentTimeMillis();
+                    qtime += t2 - t1;
+                    t1 = System.currentTimeMillis();
+                    quicksortWithoutInsertion(data1);
+                    t2 = System.currentTimeMillis();
+                    btime += t2 - t1;
+                    if (!Arrays.equals(data1, data2)) {
+                        System.out.println("FAIL TO SORT!");
+                        System.exit(0);
+                    }
+                }
+//                System.out.format("qtime: %d%nbtime: %d%n", qtime, btime);
+                System.out.println(size + "\t\t" + MAX + "\t" + 1.0 * qtime / btime);
+            }
+            System.out.println();
+        }
+    }
 }
